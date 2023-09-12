a14f34ccd53978a3c72e73e7e4adda6816b9f7e5
yousifEid <yousif.eid4@gmail.com>
2023-09-12 23:03:20 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'home',pathMatch:'full'},
+  {path:'home',component: HomeComponent},
+  {path:'about',component: AboutComponent},
+  {path:'portfolio',component: PortfolioComponent},
+  {path:'contact',component: ContactComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
